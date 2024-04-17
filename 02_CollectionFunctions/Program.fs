48fae33f74316beb8258d537cd7404bc7b8ce025
André Marcondes Teixeira <andremarcondesteixeira@gmail.com>
2024-04-17 14:52:09 -0300

    Using records and modules

--- 02_CollectionFunctions/Program.fs
+++ 02_CollectionFunctions/Program.fs
@@ -13,15 +13,34 @@ if not (File.Exists filePath) then
     printfn $"Provided file does not exist: {filePath}"
     exit 1
 
-let meanScore (row : string) =
-    let pieces = row.Split("\t")
-    let studentName = pieces.[0]
-    let studentId = pieces.[1]
-    let scores = Array.map float pieces[2..]
-    let meanScore = Array.average scores
-    let minScore = Array.min scores
-    let maxScore = Array.max scores
-    (studentName, studentId, meanScore, minScore, maxScore)
+
+// this is a record type
+type StudentScoresSummary = {
+    Name: string
+    Id: string
+    MeanScore: float
+    MinScore: float
+    MaxScore: float
+}
+
+module StudentScoresSummary =
+    let fromString (s: string): StudentScoresSummary =
+        let pieces = s.Split("\t")
+        let name = pieces.[0]
+        let id = pieces.[1]
+        let scores = Array.map float pieces[2..]
+        let meanScore = Array.average scores
+        let minScore = Array.min scores
+        let maxScore = Array.max scores
+        
+        {
+            Name = name
+            Id = id
+            MeanScore = meanScore
+            MinScore = minScore
+            MaxScore = maxScore
+        }
+    
 
 let summarize filePath =
     let lines = File.ReadAllLines filePath
@@ -31,12 +50,17 @@ let summarize filePath =
 
     lines
     |> Array.skip 1
-    |> Array.map meanScore
+    |> Array.map StudentScoresSummary.fromString
     |> Array.iter (
-        fun (studentName, studentId, meanScore, minScore, maxScore) ->
-            // the format specifier has the following format:
+        fun summary ->
+            // the %0.6f format specifier has the following format:
             // %<0 if want to pad left><total length of number including numbers after the point>.<amount of numbers after the point>
-            printfn $"{studentId}: mean: %06.2f{meanScore}, min: %06.2f{minScore}, max: %06.2f{maxScore} - {studentName}"
+            printfn "%s: mean: %06.2f, min: %06.2f, max: %06.2f - %s"
+                summary.Id
+                summary.MeanScore
+                summary.MinScore
+                summary.MaxScore
+                summary.Name
     )
 
 summarize filePath
