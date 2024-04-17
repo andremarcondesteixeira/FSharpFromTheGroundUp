06772a98059660bfdd373834fa62e5e681f0d3b6
André Marcondes Teixeira <andremarcondesteixeira@gmail.com>
2024-04-17 14:00:41 -0300

    Applies Array.min and Array.max and uses more format specifiers

--- 02_CollectionFunctions/Program.fs
+++ 02_CollectionFunctions/Program.fs
@@ -17,8 +17,11 @@ let meanScore (row : string) =
     let pieces = row.Split("\t")
     let studentName = pieces.[0]
     let studentId = pieces.[1]
-    let meanScore = pieces.[2..] |> Array.averageBy float
-    (studentName, studentId, meanScore)
+    let scores = Array.map float pieces[2..]
+    let meanScore = Array.average scores
+    let minScore = Array.min scores
+    let maxScore = Array.max scores
+    (studentName, studentId, meanScore, minScore, maxScore)
 
 let summarize filePath =
     let lines = File.ReadAllLines filePath
@@ -30,8 +33,10 @@ let summarize filePath =
     |> Array.skip 1
     |> Array.map meanScore
     |> Array.iter (
-        fun (studentName, studentId, meanScore) ->
-            printfn $"{studentId}: %0.2f{meanScore} - {studentName}"
+        fun (studentName, studentId, meanScore, minScore, maxScore) ->
+            // the format specifier has the following format:
+            // %<0 if want to pad left><total length of number including numbers after the point>.<amount of numbers after the point>
+            printfn $"{studentId}: mean: %06.2f{meanScore}, min: %06.2f{minScore}, max: %06.2f{maxScore} - {studentName}"
     )
 
 summarize filePath
