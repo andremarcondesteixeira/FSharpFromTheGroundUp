9bf8c71c0134142232773af0d2579edbe1f664ea
André Marcondes Teixeira <andremarcondesteixeira@gmail.com>
2024-06-28 19:43:07 -0300

    Extracts modules Float and StudentScoresSummary

--- 02_CollectionFunctions/Program.fs
+++ 02_CollectionFunctions/Program.fs
@@ -1,6 +1,5 @@
 ﻿open System
 open System.IO
-open System.Globalization
 
 let commandLineArgs = Environment.GetCommandLineArgs () // () is unit
 
@@ -12,57 +11,7 @@ let filePath = commandLineArgs.[1]
 
 if not (File.Exists filePath) then
     printfn $"Provided file does not exist: {filePath}"
-    exit 1
-
-module Float =
-    let tryFromString (s: string): float option =
-        let isNumber, number = Double.TryParse(s, NumberStyles.AllowDecimalPoint, CultureInfo.InvariantCulture)
-        
-        if isNumber then
-            Some(number)
-        else
-            None
-
-    let fromStringOr (defaultValue: float) (s: string) : float =
-        s |> tryFromString |> Option.defaultValue defaultValue
-
-// this is a record type
-type StudentScoresSummary = {
-    Name: string
-    Id: string
-    MeanScore: float
-    MinScore: float
-    MaxScore: float
-}
-
-module StudentScoresSummary =
-    let fromString (s: string): StudentScoresSummary =
-        let pieces = s.Split("\t")
-        let name = pieces.[0]
-        let id = pieces.[1]
-        let scores = pieces[2..] |> Array.map (Float.fromStringOr 50)
-        let meanScore = Array.average scores
-        let minScore = Array.min scores
-        let maxScore = Array.max scores
-        
-        {
-            Name = name
-            Id = id
-            MeanScore = meanScore
-            MinScore = minScore
-            MaxScore = maxScore
-        }
-
-    let printSummary (summary: StudentScoresSummary) =
-        // the %0.6f format specifier has the following format:
-        // %<0 if want to pad left><total length of number including numbers after the point>.<amount of numbers after the point>
-        printfn "%s: mean: %06.2f, min: %06.2f, max: %06.2f - %s"
-            summary.Id
-            summary.MeanScore
-            summary.MinScore
-            summary.MaxScore
-            summary.Name
-    
+    exit 1    
 
 let summarize filePath =
     let lines = File.ReadAllLines filePath
